f737590464c0d4d79b801baf2452a27aa26c9c61
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-30 03:48:50 +0000

    Insertion sort

--- sort.h
+++ sort.h
@@ -4,13 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ASCENDING 0
-#define DESCENDING 1
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 
 /**
- * enum bool - Checks using boolean values.
- * @false: Represents the value 0.
- * @true: Represents the value 1.
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
  */
 typedef enum bool
 {
@@ -32,13 +33,15 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -46,5 +49,6 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
