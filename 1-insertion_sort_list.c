7e459845c3e2485a3ba7bc044d38a3cc89048d3e
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-30 04:21:19 +0000

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,49 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - a C function that sorts a list using insertion
- * sorting algorithm
- * @list: A pointer to the head of list of integers
+ * list_swapper - a c function that swap nodes in a list
+ * @h: list head
+ * @n1: first node
+ * @n2: second node
+ */
+void list_swapper(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+
+	else
+		*h = n2;
+
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - a C function that sort lists using the insertion sort
+ * @list: the list
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *iter, *insert, *swapped;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *sorted = NULL;
-	listint_t *current = *list;
-
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = swapped)
 	{
-		listint_t *next = current->next;
+		swapped = iter->next;
+		insert = iter->prev;
 
-		if (sorted == NULL || sorted->n >= current->n)
+		while (insert != NULL && iter->n < insert->n)
 		{
-			current->next = sorted;
-			current->prev = NULL;
-			if (sorted != NULL)
-				sorted->prev = current;
-			sorted = current;
+			list_swapper(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		else
-		{
-			listint_t *insert_pos = sorted;
-
-			while (insert_pos->next != NULL && insert_pos->next->n
-					< current->n)
-				insert_pos = insert_pos->next;
-
-			current->next = insert_pos->next;
-			if (insert_pos->next != NULL)
-				insert_pos->next->prev = current;
-			insert_pos->next = current;
-			current->prev = insert_pos;
-		}
-		current = next;
 	}
-
-	*list = sorted;
 }
